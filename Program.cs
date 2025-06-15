968537d109ce0587210b5ed5421d39438dd60cf3
Janek <janpawlakofficial@gmail.com>
2025-06-15 22:40:25 +0200

    Published into exe

--- Program.cs
+++ Program.cs
@@ -9,6 +9,11 @@ internal static class Program
     [STAThread]
     static void Main()
     {
+        using (var db = new AppDbContext())
+        {
+            db.Database.EnsureCreated(); // creates DB and tables if missing
+        }
+
         ApplicationConfiguration.Initialize();
         Application.Run(new MainForm());
     }
--- obj/Debug/net8.0-windows/XpenseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/XpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c04dfc0ab2ef8f213aebac88c2225f405f3e182f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+558e1a968cd9ca21f71293609ffd45b348861ce0")]
 [assembly: System.Reflection.AssemblyProductAttribute("XpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
